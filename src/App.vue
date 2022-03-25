1530bd40514f200ff8e7523a9f8d8161134cce08
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-25 17:16:27 +0100

    Creazione app con Vue e inserimento title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyVueTitle msg="La mia prima app con Vue CLI!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MyVueTitle from "./components/MyVueTitle.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MyVueTitle,
   },
 };
 </script>
